6639a3a5858bff7208ab799aa516c1c447529d21
Louis-Mont <Louis-Mont@users.noreply.github.com>
2022-09-01 16:49:50 +0200

    Reworked test

--- tests/test_Replication.py
+++ tests/test_Replication.py
@@ -1,18 +1,22 @@
+from tkinter import Tk
 from unittest import TestCase
 from Replication import Replication
 from db_interface.DatabaseODBC import DatabaseODBC
+from user_interface.UI import UI
 from write_tables_csv import tables
 from main import find_table
 from pypyodbc import Error
 
 
 class TestReplication(TestCase):
-    def test_replicate(self):
+    def test_replicate_client(self):
         home = DatabaseODBC()
         home.connect("gdr")
-        replication = Replication()
+        replication = Replication(UI(Tk(), []))
+        t_name = "Client"
         try:
-            replication.replicate(home, home, "Client", tables[find_table("Client", tables)][1], "31082019")
-            self.assertEqual(1, 1)
+            replicated = replication.replicate(home, home, t_name, tables[find_table(t_name, tables)][1],
+                                               "31082019")
+            self.assertIs(None, replicated)
         except Error as Er:
             self.fail(Er)
